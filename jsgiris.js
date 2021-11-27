cca95b9031c6b378e975c1d4670e243f32c8c91e
Hale Günay <89314705+halegny@users.noreply.github.com>
2021-11-27 10:27:37 +0300

    Add files via upload

---
+++ jsgiris.js
@@ -0,0 +1,18 @@
+
+let isim=prompt("İsminizi giriniz")
+
+let anaEkran = document.querySelector("#style")
+
+anaEkran.innerHTML=` Merhaba, <small style="color:red">${isim}</small>! Hoş geldin!`
+
+//${}
+
+
+
+
+
+
+
+
+
+
